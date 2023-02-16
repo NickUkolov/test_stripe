e8cd66f25f33691b7a82e93bd6f0e9237f39d4d5
NickUkolov <ukolov_1996@list.ru>
2023-02-16 19:02:36 +0300

    error

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from app.models import Item
+from app.models import Item, Order
 
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    fields = ('name', 'description', 'price', 'currency', 'order')
+
+class ItemInline(admin.StackedInline):
+    model = Item
+    extra = 1
+
+
+class OrdersAdmin(admin.ModelAdmin):
+    # inlines = [ItemInline,]
+    fields = ('name', 'paid')
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Order, OrdersAdmin)
--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class Item(models.Model):
     description = models.CharField(max_length=256)
     price = models.IntegerField(default=0)
     currency = models.CharField(max_length=3, blank=True, null=True)
+    order = models.ForeignKey('Order', related_name='item', on_delete=models.SET_NULL, blank=True, null=True)
 
     class Meta:
         verbose_name = 'Товар'
@@ -17,3 +18,27 @@ class Item(models.Model):
 
     def get_display_price(self):
         return f'{self.price/100:.2f}'
+
+
+class Order(models.Model):
+
+    name = models.CharField(max_length=128, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, default=None)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    # def sum_price(self):
+    #     price = []
+    #     items = self.item.all()
+    #     for item in items:
+    #         price.append(int(item.price))
+    #     return sum(price)/100
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    count = models.IntegerField(default=1)
